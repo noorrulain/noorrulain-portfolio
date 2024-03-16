7e88ed6dff5228178080646b1d0647eeb8443b12
noorrulain <noor25pk5@gmail.com>
2024-03-16 16:50:17 +0500

    Make responsive for all screens

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-// $("li").mouseenter((this) => {
-//     $(this).addClass("active");
-// })
+var skills = document.getElementById("skill-stack");
+
+var mq = window.matchMedia( "(max-width: 600px)" );
+if (mq.matches) {
+    skills.innerHTML=`<img
+    src="https://skillicons.dev/icons?i=html,css,js,jquery,react"
+  /><br><br><img src="https://skillicons.dev/icons?i=nodejs,express,vscode,bootstrap,git" />`
+}
+
+// https://skillicons.dev/icons?i=html,css,js,jquery,react,nodejs,express,vscode,bootstrap,git,postgres
